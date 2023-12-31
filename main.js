d06c231ffe297391258c1ba3a3383e3d212bef73
Ahnaf Kamal <ahnaf.kamalx@gmail.com>
2023-12-30 19:57:10 -0500

    save point

--- main.js
+++ main.js
@@ -1,31 +1,22 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-let scene,
-  camera,
-  fieldOfView,
-  aspectRatio,
-  nearPlane,
-  farPlane,
-  renderer,
-  container,
-  rocket,
-  HEIGHT,
-  WIDTH,
-  DIVIDEAMOUNT;
+let scene, camera, canvas, fieldOfView, aspectRatio, nearPlane, farPlane, renderer, rocket, HEIGHT, WIDTH, DIVIDEAMOUNT;
+
+// makes the scene
 const createScene = () => {
+
   HEIGHT = window.innerHeight;
   WIDTH = window.innerWidth;
-  DIVIDEAMOUNT = 1.3
+  DIVIDEAMOUNT = 1.5;
   scene = new THREE.Scene();
-
-  scene.fog = new THREE.Fog(0xffffff, 10, 1200);
-  //scene.background = new THREE.Color(0xff0000)
-
+  canvas = document.querySelector('#rocket-view-container');
   aspectRatio = WIDTH / (HEIGHT/DIVIDEAMOUNT);
   fieldOfView = 60;
   nearPlane = 1;
   farPlane = 10000;
+  
+  // Defining the camera
   camera = new THREE.PerspectiveCamera(
     fieldOfView,
     aspectRatio,
@@ -33,39 +24,39 @@ const createScene = () => {
     farPlane
   );
 
+  // Defining the renderer
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+    canvas
+  });
+
+  // Defining the loader
+  const loader = new GLTFLoader();
+  loader.load( "models/rocket.glb",
+    (gltf) => {
+      rocket = gltf.scene;
+      rocket.position.x = -85;
+      rocket.position.y = -15;
+      rocket.position.z = 50;
+      scene.add(rocket);
+    }
+  );
+
   camera.position.x = 0;
   camera.position.z = 300;
   camera.position.y = -5;
 
-  renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    antialias: true
-  });
-  
+  scene.fog = new THREE.Fog(0xffffff, 10, 1200);
+  //scene.background = new THREE.Color(0xff0000)
   renderer.setSize(WIDTH, (HEIGHT/DIVIDEAMOUNT));
   renderer.shadowMap.enabled = true;
 
-  container = document.getElementById("rocket-view-container")
-  container.appendChild(renderer.domElement)
-  //document.body.appendChild( renderer.domElement );
+  window.addEventListener("resize", handleWindowResize, false); // on resize
 
-  window.addEventListener("resize", handleWindowResize, false);
-
-  let loader = new GLTFLoader();
-  loader.load( "models/rocket.glb",
-    (gltf) => {
-      rocket = gltf.scene;
-      rocket.position.x = -100;
-      //rocket.position.y = 100;
-      //rocket.position.x = 100;
-      //rocket.position.z = 100;
-      //rocket.rotation.x = 90;
-      //rocket.rotation.z = 90;
-      scene.add(rocket);
-    }
-  );
 };
 
+// Handles what happens when the window resizes
 const handleWindowResize = () => {
   HEIGHT = window.innerHeight;
   WIDTH = window.innerWidth;
@@ -74,13 +65,13 @@ const handleWindowResize = () => {
   camera.updateProjectionMatrix();
 };
 
+// Creates lights
 const createLights = () => {
   const ambientLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
-
   const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
-  directionalLight.position.set(-100, 100, 600);
-
   const pointLight = new THREE.PointLight(0xffffff, 2, 2000, 3);
+
+  directionalLight.position.set(-100, 100, 600);
   pointLight.position.set(200, -100, 50);
 
   scene.add(ambientLight, directionalLight, pointLight);
@@ -89,11 +80,10 @@ const createLights = () => {
 const targetRocketPosition = 10;
 const animationDuration = 2000;
 
+// Loop 
 const loop = () => {
   const t = (Date.now() % animationDuration) / animationDuration;
-
   renderer.render(scene, camera);
-
   const delta = targetRocketPosition * Math.sin(Math.PI * 2 * t);
   if (rocket) {
     //rocket.rotation.y += 0.1;
@@ -103,12 +93,12 @@ const loop = () => {
   }
 
   requestAnimationFrame(loop);
+
 };
 
 const main = () => {
   createScene();
   createLights();
-
   renderer.render(scene, camera);
   loop();
 };
